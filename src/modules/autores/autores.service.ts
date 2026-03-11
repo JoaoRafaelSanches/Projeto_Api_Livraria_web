84993c2090007e711f1d6ad1920411b8464bc822
João Rafael Sanches <jsanchesedantas@gmail.com>
2026-03-10 22:47:20 -0300

    q

--- src/modules/autores/autores.service.ts
+++ src/modules/autores/autores.service.ts
@@ -1,74 +1,37 @@
-<<<<<<< HEAD
 import { Injectable } from '@nestjs/common';
+import { ok } from 'assert';
 
 let autores = [
   {
     id: 1,
-    nome: 'João da Silva',
-    email: 'joao.silva@gmail.com',
+    nome: 'brayan martins',
+    email: 'brayan@gmail.com',
   },
+
   {
     id: 2,
-    nome: 'Maria Oliveira',
-    email: 'maria.oliveira@gmail.com',
+    nome: 'lucas martins',
+    email: 'lucas@gmail.com',
   },
   {
     id: 3,
-    nome: 'Pedro Santos',
-    email: 'pedro.santos@gmail.com',
+    nome: 'Midiel martins',
+    email: 'Midiel@gmail.com',
   },
 ];
 
 @Injectable()
-export class AutoresService {
-  listarAutores() {
+export class AutoresSevice {
+  ListarAutores() {
+    //return "brayan martins "
     return autores;
   }
 
-  listarAutor(id: number) {
+  ListarAutor(id: number) {
     const autorEncontrado = autores.find((autor) => autor.id === id);
-
     if (!autorEncontrado) {
-      return 'Autor não encontrado';
+      return 'Nao encontrado!';
     }
     return autorEncontrado;
   }
 }
-=======
-import { Injectable } from "@nestjs/common";
-import { ok } from "assert";
-
-let autores = [{
-     id:1 
-    ,nome :"brayan martins"
-    ,email: "brayan@gmail.com"
-},
-
-{
-    id:2 
-    ,nome :"lucas martins"
-    ,email: "lucas@gmail.com" 
-},
-{
-    id:3 
-    ,nome :"Midiel martins"
-    ,email: "Midiel@gmail.com"
-}
-]
-
-@Injectable()
-export class AutoresSevice{
-    ListarAutores(){
-        //return "brayan martins "
-        return autores;
-    }
-
-    ListarAutor(id: number){
-        const autorEncontrado = autores.find((autor) => autor.id === id);
-         if( !autorEncontrado){
-            return "Nao encontrado!" 
-         }
-        return  autorEncontrado; 
-    }
-}
->>>>>>> c28f00821b50d6e71dff95df2871167a33882551
